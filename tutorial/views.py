73643abec16fc40b65c75547760c7ce141865168
slickhire <63951836+slickhire@users.noreply.github.com>
2020-05-03 15:32:36 -0400

    add calendar

--- tutorial/views.py
+++ tutorial/views.py
@@ -5,6 +5,75 @@ from tutorial.forms import StudentForm
 from django.core.serializers import serialize
 from django.http import JsonResponse
 from . import models
+from django.views.decorators.csrf import csrf_exempt
+
+def jprofile(request):
+   return render(request, "jprofile.html")
+@csrf_exempt
+def deleteJob(request):
+    try:
+        data = models.JobProfile.objects.get(jobId__exact=request.POST['jobid'])
+        data.delete()
+    except models.Person.DoesNotExist:
+        print("")
+    return HttpResponse("success")
+
+def getJob(request):
+    data = list(models.JobProfile.objects.values_list())
+    return JsonResponse({"draw": 1, "recordsTotal": 1, "recordsFiltered": 1, "data": data}, safe=False)
+
+
+def calendarCandidate(request):
+    if request.method == 'GET':
+        id = request.GET["id"]
+        if id == "":
+            return HttpResponse("Invalid Request")
+        try:
+            data = models.Person.objects.only('questions').get(stringId__exact=request.GET['id'])
+            row = models.JobProfile.objects.get(jobId__exact=data.questions)
+
+        except models.Person.DoesNotExist:
+            return HttpResponse("Invalid Request")
+
+        
+        return render(request, "calendarCandidate.html", {'data': "[]" if row.calendar == "" else row.calendar, 'uid': request.GET['id']})
+
+@csrf_exempt
+def calendar(request):
+    if request.method == 'GET':
+        id = request.GET["id"]
+        if id == "":
+            return HttpResponse("Invalid Request")
+        row = models.JobProfile.objects.get(jobId__exact=request.GET['id'])
+        return render(request, "calendar.html", {'data': "[]" if row.calendar == "" else row.calendar, 'jobId': request.GET['id']})
+    else:
+        if request.POST['id'] != "":
+            id = request.POST['id']
+            row = models.JobProfile.objects.get(jobId__exact=id)
+            row.calendar = request.POST['calendar']
+            row.save()
+        else:
+            uid = request.POST['uid']
+            user = models.Person.objects.get(stringId__exact=request.POST['uid'])
+            id = user.questions
+            row = models.JobProfile.objects.get(jobId__exact=id)
+            newVal = request.POST['calendar']
+            newVal = newVal.replace("#FFFFFF", "#FFA07A");
+            row.calendar = row.calendar.replace(request.POST['calendar'], newVal, 1)
+            row.save()
+        return HttpResponse("success")
+
+
+@csrf_exempt
+def saveJob(request):
+    print("save")
+    salary = request.POST['salary'].split('-')
+    exp = request.POST['exp'].split('-')
+    notice = request.POST['notice'].split('-')
+
+    job = models.JobProfile(jobId=request.POST['jobid'], designation=request.POST['designation'], experience = request.POST['exp'], salary = request.POST['salary'], notice = request.POST['notice'], skills = request.POST['skills'], salary1 = salary[0], salary2 = salary[1], exp1 = exp[0], exp2 = exp[1], notice1 = notice[0], notice2 = notice[1])
+    job.save()
+    return HttpResponse("success")
 
 def index(request):  
     if request.method == 'POST': 
@@ -38,30 +107,36 @@ def questions(request):
     if request.method == 'POST':
         data = models.Person.objects.only('questions').get(stringId__exact=request.POST['strId'])
         data.status = "Received"
-        questions = models.Questions.objects.filter(key__exact=data.questions)
+        row = models.JobProfile.objects.get(jobId__exact=data.questions)
         score = 0
-        for q in questions:
-            print("hello", q.tagId)
-            if request.POST[q.tagId] != "":
-                if q.tagId == "experience":
-                    data.experience = request.POST[q.tagId]
-                    if int(q.expected1) <= int(data.experience) <= int(q.expected2):
-                        score += 50
-                    else:
-                        score += GetScore(int(50), q.expected1, q.expected2, data.experience)  
-                if q.tagId == "company":
-                    data.employer = request.POST[q.tagId]
-                if q.tagId == "currentCtc":
-                    data.salary = request.POST[q.tagId]
-                if q.tagId == "expectedCtc":
-                    data.expectedCtc = request.POST[q.tagId]
-                    data.salary = data.expectedCtc
-                    if int(q.expected1) <= int(data.expectedCtc) <= int(q.expected2):
-                        score += 50
-                    elif int(q.expected1) > int(data.expectedCtc) :
-                        score += 50
-                    else:
-                        score += GetScore(int(50), q.expected1, q.expected2, data.expectedCtc) 
+        data.experience = request.POST['experience']
+        data.salary = request.POST['currentCtc']
+        data.employer = request.POST['company']
+        data.expectedCtc = request.POST['expectedCtc']
+        data.notice = request.POST['notice']
+        skills = row.skills.split(',')
+        weightPerQuestion = 100 / (3 + len(skills))
+        if int(row.exp1) <= int(data.experience) <= int(row.exp2):
+            score += weightPerQuestion 
+        else:
+            score += GetScore(int(weightPerQuestion), q.expected1, q.expected2, data.experience)
+        
+        if int(row.salary1) <= int(data.expectedCtc) <= int(row.salary2):
+            score += weightPerQuestion
+        elif int(row.salary1) > int(data.expectedCtc) :
+            score += weightPerQuestion
+        else:
+            score += GetScore(int(weightPerQuestion), row.salary1, row.salary2, data.expectedCtc)
+   
+        if int(row.notice1) <= int(data.notice) <= int(row.notice2):
+            score += weightPerQuestion
+        elif int(row.notice1) > int(data.notice):
+            score += weightPerQuestion
+        else:
+            score += GetScore(int(weightPerQuestion), row.notice1, row.notice2, data.notice)
+
+        for q in skills:
+            score += (int(request.POST[q]) * weightPerQuestion) / 10
         data.score = score
         data.save()
         return HttpResponse("Data submitted successfuly" + request.POST['strId'])
@@ -75,7 +150,12 @@ def questions(request):
         except models.Person.DoesNotExist:
             return HttpResponse("Invalid Request")
 
-        return render(request,"questions.html", {'qs': list(models.Questions.objects.filter(key__exact=data.questions)), 'stringId': id})
+        row = models.JobProfile.objects.only('skills').get(jobId__exact=data.questions)
+        skillList = []
+        for x in row.skills.split(','):
+            skillList.append(x)
+
+        return render(request,"questions.html", {'qs': skillList, 'stringId': id})
 
 def opt_out(request):
     if request.method == 'POST': 
@@ -90,7 +170,9 @@ def opt_out(request):
         print('Receieved GET opt-out request for: ', request.GET["id"])
         return render(request,"optout.html", {'stringId': request.GET["id"]})
 
+
 def homepage(request):
     return HttpResponse("First App")
 
 # Create your views here.
+
